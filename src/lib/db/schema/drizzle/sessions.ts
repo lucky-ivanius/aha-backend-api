60654275ff244d1e2259226cf63d52f8d3a1ac3f
Lucky Ivanius <luckzivanius@gmail.com>
2025-02-27 09:55:44 +0700

    feat: add indexes on drizzle ts schema

--- src/lib/db/schema/drizzle/sessions.ts
+++ src/lib/db/schema/drizzle/sessions.ts
@@ -1,33 +1,43 @@
 import { relations } from "drizzle-orm";
 import {
-  pgTable,
-  varchar,
   boolean,
-  timestamp,
+  index,
+  pgTable,
   text,
+  timestamp,
   uuid,
+  varchar,
 } from "drizzle-orm/pg-core";
+import { v7 } from "uuid";
 
-import { generateRandomHex } from "../../../token/hex";
-
-import { users } from "./users";
 import { timestamps } from "./timestamps";
+import { users } from "./users";
 
 // Table
-export const sessions = pgTable("sessions", {
-  id: varchar()
-    .$defaultFn(() => generateRandomHex(32))
-    .primaryKey(),
-  userId: uuid()
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  ipAddress: varchar({ length: 45 }).notNull(),
-  userAgent: text(),
-  isRevoked: boolean().default(false).notNull(),
-  lastActiveAt: timestamp().defaultNow().notNull(),
-  expiresAt: timestamp().notNull(),
-  ...timestamps,
-});
+export const sessions = pgTable(
+  "sessions",
+  {
+    id: uuid()
+      .$defaultFn(() => v7())
+      .primaryKey(),
+    userId: uuid()
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    ipAddress: varchar({ length: 45 }).notNull(),
+    userAgent: text(),
+    isRevoked: boolean().default(false).notNull(),
+    lastActiveAt: timestamp().defaultNow().notNull(),
+    expiresAt: timestamp().notNull(),
+    ...timestamps,
+  },
+  (session) => [
+    index("active_sessions_idx").on(
+      session.userId,
+      session.isRevoked,
+      session.expiresAt,
+    ),
+  ],
+);
 
 // Relations
 export const sessionsRelations = relations(sessions, ({ one }) => ({
--- src/lib/db/schema/drizzle/user-provider.ts
+++ src/lib/db/schema/drizzle/user-provider.ts
@@ -1,20 +1,29 @@
 import { relations } from "drizzle-orm";
-import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, index, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 import { timestamps } from "./timestamps";
 import { users } from "./users";
 
 // Table
-export const userProviders = pgTable("user_providers", {
-  id: uuid().defaultRandom().primaryKey(),
-  userId: uuid()
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  provider: varchar({ length: 255 }).notNull(),
-  externalId: varchar().notNull(),
-  passwordEnabled: boolean().notNull(),
-  ...timestamps,
-});
+export const userProviders = pgTable(
+  "user_providers",
+  {
+    id: uuid().defaultRandom().primaryKey(),
+    userId: uuid()
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    provider: varchar({ length: 255 }).notNull(),
+    externalId: varchar().notNull(),
+    passwordEnabled: boolean().notNull(),
+    ...timestamps,
+  },
+  (userProvider) => [
+    index("provider_lookup_idx").on(
+      userProvider.provider,
+      userProvider.externalId,
+    ),
+  ],
+);
 
 // Relations
 export const userProvidersRelations = relations(userProviders, ({ one }) => ({
