70767a5100e516fa5f2f0e38547a32c387014b00
Lucky Ivanius <luckzivanius@gmail.com>
2025-02-27 07:08:00 +0700

    fix: check postgresql connection on startup

--- src/lib/db/drizzle.ts
+++ src/lib/db/drizzle.ts
@@ -1,9 +1,35 @@
+import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 import env from "../../config/env";
 
+import logger from "../../utils/logger";
 import schema from "./schema/drizzle";
 
-const db = drizzle(env.PG_DATABASE_URL, { schema, casing: "snake_case" });
+const db = drizzle(env.PG_DATABASE_URL, {
+  schema,
+  casing: "snake_case",
+});
+
+const checkConnection = async () => {
+  try {
+    const [result] = (
+      await db.execute<{ current_database: string }>(
+        sql`SELECT current_database();`,
+      )
+    ).rows;
+
+    logger.info("Established connection to database", {
+      current_database: result.current_database,
+    });
+  } catch (error) {
+    delete (error as Error).stack;
+
+    logger.error(error);
+    process.exit(1);
+  }
+};
+
+checkConnection();
 
 export default db;
