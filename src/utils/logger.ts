c2415bb6fd299911f64ad13bdb6f957dd674704d
Lucky Ivanius <luckzivanius@gmail.com>
2025-03-01 02:34:31 +0700

    fix: remove file logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -29,24 +29,24 @@ const logger = winston.createLogger({
   ],
 });
 
-if (process.env.NODE_ENV === "production") {
-  logger.add(
-    new winston.transports.File({
-      filename: "logs/error.log",
-      level: "error",
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-    }),
-  );
+// if (process.env.NODE_ENV === "production") {
+//   logger.add(
+//     new winston.transports.File({
+//       filename: "logs/error.log",
+//       level: "error",
+//       maxsize: 5242880, // 5MB
+//       maxFiles: 5,
+//     }),
+//   );
 
-  logger.add(
-    new winston.transports.File({
-      filename: "logs/combined.log",
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-    }),
-  );
-}
+//   logger.add(
+//     new winston.transports.File({
+//       filename: "logs/combined.log",
+//       maxsize: 5242880, // 5MB
+//       maxFiles: 5,
+//     }),
+//   );
+// }
 
 export const attachRequestId = (requestId: string) => {
   return logger.child({ requestId });
