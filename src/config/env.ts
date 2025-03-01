6ceea605f8bc9db4287f96f9bf691f6d292f89ac
Lucky Ivanius <luckzivanius@gmail.com>
2025-03-02 00:30:38 +0700

    fix: set zod invalid tz to UTC by default

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,13 +4,16 @@ import { z } from "zod";
 config();
 
 const envSchema = z.object({
-  TZ: z.string().transform((value) => {
-    const isValidTz = Intl.DateTimeFormat()
-      .resolvedOptions()
-      .timeZone.includes(value);
+  TZ: z
+    .string()
+    .default("UTC")
+    .transform((value) => {
+      const isValidTz = Intl.DateTimeFormat()
+        .resolvedOptions()
+        .timeZone.includes(value);
 
-    return isValidTz ? value : "UTC";
-  }),
+      return isValidTz ? value : "UTC";
+    }),
   NODE_ENV: z
     .enum(["development", "test", "production"])
     .default("development"),
